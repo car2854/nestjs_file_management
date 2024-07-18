7d2f3a08f989ee71be6cf7f845a470a48e8ff445
car2854 <car2854@gmail.com>
2024-07-18 12:53:49 -0400

    cambiando el porty

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
   app.setGlobalPrefix('api');
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
